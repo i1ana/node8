d6626c38290fcedfb57424141d71f2cf5f036233
Ilana <bachwasbaroque@gmail.com>
2016-02-23 12:31:34 -0700

    lunch time

--- index.js
+++ index.js
@@ -2,14 +2,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var mongoose = require('mongoose')
 
 // Create Express App Object \\
 var app = express();
+mongoose.connect('mongodb://localhost/jobapp')
 
 // Application Configuration \\
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+
+
+var jobAppSchema = mongoose.Schema({
+	name	: {type: String},
+	bio 	: String,
+	skills	: {type: Array},
+	years	: {type: Number},
+	why		: String,
+})
+
+//MODEL COLLECTION 
+var JobApp = mongoose.model('JobApp', jobAppSchema)
+
+var yob = new JobApp({
+	name	: req.body.name,
+	bio: 'an adventuror',
+	skills: ['walking', 'carrying rings', 'being small'],
+	years: 13,
+	why: 'looking for a new mission'
+})
+
+yob.save(function(err, document){
+	console.log('Error: ', err)
+	console.log('Docu: ', document)
+	res.send(document)
+})
 
 // Routes \\
 
@@ -20,16 +48,20 @@ app.get('/', function(req, res) {
 // displays a list of applicants
 app.get('/applicants', function(req, res){
 	res.sendFile('html/applicants.html', {root : './public'});
-	console.log('testing')
 });
 
 // creates and applicant
 app.post('/applicant', function(req, res){
 	// Here is where you need to get the data
 	// from the post body and store it in the database
-	res.send('No function');
+	res.send('Success!')
+	console.log(req.body)
 });
 
+app.get('/success', function(req, res){
+	res.redirect('/success')
+
+})
 
 
 // Creating Server and Listening for Connections \\
